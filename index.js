8371ee9082c9f794c2b11d0b096d3a46eacff7f9
thelewisndegwa <lewiskanyi77@gmail.com>
2023-04-04 14:53:25 +0300

    commit 4?

--- index.js
+++ index.js
@@ -38,3 +38,52 @@ function showSlides(n) {
 setInterval(() => {
   plusSlides(1);
 }, 3000);
+
+
+//fetch and display items to flip cards
+
+const cardsContainer = document.getElementById('cards-container');
+
+fetch('db.json')
+  .then(response => response.json())
+  .then(data => {
+    const accommodations = data.accommodations;
+    for (const accommodation of accommodations) {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      
+      const flipper = document.createElement('div');
+      flipper.classList.add('flipper');
+      
+      const front = document.createElement('div');
+      front.classList.add('front');
+      front.textContent = accommodation.type;
+      
+      const back = document.createElement('div');
+      back.classList.add('back');
+      const price = document.createElement('p');
+      price.textContent = `Price per night: $${accommodation.price_per_night}`;
+      const rooms = document.createElement('p');
+      rooms.textContent = `Available rooms: ${accommodation.available_rooms}`;
+      const occupancy = document.createElement('p');
+      occupancy.textContent = `Max occupancy: ${accommodation.max_occupancy}`;
+      const description = document.createElement('p');
+      description.textContent = accommodation.description;
+      const type = document.createElement('h2');
+      type.textContent = accommodation.type;
+      
+      back.appendChild(type);
+      back.appendChild(price);
+      back.appendChild(rooms);
+      back.appendChild(occupancy);
+      back.appendChild(description);
+      
+      flipper.appendChild(front);
+      flipper.appendChild(back);
+      
+      card.appendChild(flipper);
+      
+      cardsContainer.appendChild(card);
+    }
+  })
+  .catch(error => console.error(error));
