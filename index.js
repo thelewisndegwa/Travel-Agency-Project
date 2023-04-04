92f86d7b2d350d3056ce345bd3b50b8191ec0803
thelewisndegwa <lewiskanyi77@gmail.com>
2023-04-04 11:47:04 +0300

    second commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// Thumbnail image controls 
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
